341ffd6752a143660f054247ee52428f34042d2a
Florian Kate <s51541@bht-berlin.de>
2022-11-24 22:17:42 +0100

    merged trip manager and gps reciever

--- GPSTripManager/app/src/main/java/com/example/gps_tripmanager/MainActivity.kt
+++ GPSTripManager/app/src/main/java/com/example/gps_tripmanager/MainActivity.kt
@@ -5,6 +5,10 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -12,16 +16,15 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.provider.DocumentsContract
 import android.provider.Settings
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.io.BufferedReader
@@ -29,8 +32,6 @@ import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 
@@ -267,5 +268,77 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
     companion object {
         const val READ_REQUEST_CODE = 123
+    }
+
+    private fun getUTM(latitude: Double, longitude: Double) : String {
+        val ll4 = LatLng(latitude, longitude)
+        val utm2 = ll4.toUTMRef()
+        return utm2.toString()
+    }
+
+    private fun drawRoute() {
+        // init
+        var minLon: Double = 181.0
+        var maxLon: Double = -181.0
+        var minLat: Double = 91.0
+        var maxLat: Double = -91.0
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        val bitmap = Bitmap.createBitmap(imageView.width, imageView.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val paint = Paint()
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 3F
+        paint.isAntiAlias = true
+        imageView.setImageBitmap(bitmap)
+        val width = bitmap.width
+        val height = bitmap.height
+
+        // draw 5 x 5 grid
+        var x = 1
+        var y = 1
+        while(x < 5) {
+            while(y < 5) {
+                canvas.drawLine((y*height/5.0).toFloat(), 0.0f, (y*height/ 5.0).toFloat(), height.toFloat(), paint)
+                y++
+            }
+            canvas.drawLine(0.0f, (x*width/5.0).toFloat(), width.toFloat(), (x*width/ 5.0).toFloat(), paint)
+            x++
+        }
+
+        // set max and min longitude and latitude for appropriate scaling
+        for(trackpoint in trackpoints) {
+            if(trackpoint.longitude < minLon) minLon = trackpoint.longitude
+            if(trackpoint.longitude > maxLon) maxLon = trackpoint.longitude
+            if(trackpoint.latitude < minLat) minLat = trackpoint.latitude
+            if(trackpoint.latitude > maxLat) maxLat = trackpoint.latitude
+        }
+        // set display units
+        var deltaLon = maxLon - minLon
+        var deltaLat = maxLat - minLat
+        var dpx = width / deltaLon
+        var dpy = height / deltaLat
+
+        Log.i("UTM min", getUTM(minLat, minLon))
+        Log.i("UTM max", getUTM(maxLat, maxLon))
+
+        var i = 0
+        while(i < trackpoints.size - 2) {
+            var split_utms_first = getUTM(trackpoints[i].latitude, trackpoints[i].longitude).split(" ")
+            var split_utms_second = getUTM(trackpoints[i + 1].latitude, trackpoints[i + 1].longitude).split(" ")
+            var xCordsFirst = split_utms_first[1].split(".")[0].takeLast(3)
+            var yCordsFirst = split_utms_first[2].split(".")[0].takeLast(3)
+            var xCordsSecond = split_utms_second[1].split(".")[0].takeLast(3)
+            var yCordsSecond = split_utms_second[2].split(".")[0].takeLast(3)
+
+            var x1 = ((xCordsFirst.toDouble() * dpx) - minLon).toFloat()
+            var y1 = ((yCordsFirst.toDouble() * dpy) - minLat).toFloat()
+            var x2 = ((xCordsSecond.toDouble() * dpx) - minLon).toFloat()
+            var y2 = ((yCordsSecond.toDouble() * dpy) - minLat).toFloat()
+
+            Log.i("for i=$i", "x1: $x1 | x2: $x2 | y1: $y1 | y2: $y2")
+            canvas.drawLine(x1, y1, x2, y2, paint)
+            i++
+        }
     }
 }
