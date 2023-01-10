e2761dec21a8270a83058bb3f0bd111b7e571b2b
Florian Kate <s51541@bht-berlin.de>
2023-01-10 22:03:51 +0100

    hikeroute - some build related fixes

--- Hikeroute/app/src/main/java/com/example/hikeroute/RouteDatabase.kt
+++ Hikeroute/app/src/main/java/com/example/hikeroute/RouteDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [RouteEntity::class], version = 1)
 abstract class RouteDatabase: RoomDatabase() {
@@ -19,22 +18,10 @@ abstract class RouteDatabase: RoomDatabase() {
             if(instance == null)
                 instance = Room.databaseBuilder(ctx.applicationContext, RouteDatabase::class.java, "route_database")
                     .fallbackToDestructiveMigration()
-                    .addCallback(roomCallback)
                     .build()
 
             return instance!!
         }
     }
 
-    private val roomCallback = object : Callback() {
-        override fun onCreate(db: SupportSQLiteDatabase) {
-            super.onCreate(db)
-            populateDatabase(instance!!)
-        }
-    }
-
-    private fun populateDatabase(db: RouteDatabase) {
-        val routeDao = db.routeDao()
-        // if needed test data can be created here
-    }
 }
